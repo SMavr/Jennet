a20bc01d2dee905bd31cd6b7ae6c4cac18d560e6
Stam <smavrikis@benefit.gr>
2022-02-26 17:51:25 +0200

    Started with wpf

---
+++ Jennet.Solution/Jennet.UI/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jennet.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string FilePath = string.Join(" ", Environment.GetCommandLineArgs().Skip(1).ToArray());
+            if (!string.IsNullOrEmpty(FilePath))
+            {
+                string FileContent = System.IO.File.ReadAllText(FilePath);
+                textBox.Text = FileContent;
+            }
+        }
+    }
+}
