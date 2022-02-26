1d0fc3870a114e801f14c47d84bbf25fa01e42ba
Stam <smavrikis@benefit.gr>
2022-02-26 18:17:11 +0200

    Fixed test

--- Jennet.Solution/Jennet.Tests/UnitTest1.cs
+++ Jennet.Solution/Jennet.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Text.Json;
 using Xunit;
 
 namespace Jennet.Tests
@@ -8,18 +10,37 @@ namespace Jennet.Tests
 @"
 {
   ""ReRoutes"":[
-{
-      ""UpstreamPathTemplate"":""test"" 
-      ""DownstreamPathTemplate"": ""test""
-}
-  ]
+   {
+      ""UpstreamPathTemplate"":""test"", 
+     ""DownstreamPathTemplate"": ""test""
+    },
+    {
+          ""UpstreamPathTemplate"":""test1"", 
+          ""DownstreamPathTemplate"": ""test1""
+    }
+]
+
 }
 ";
 
         [Fact]
         public void Test1()
         {
-
+            var test =  JsonSerializer.Deserialize<OcelotResult>(jsonString);
         }
+
+        private class Route
+        {
+            public string UpstreamPathTemplate { get; set; }
+            public string DownstreamPathTemplate { get; set; }
+        }
+
+        private class OcelotResult
+        {
+            public List<Route> ReRoutes { get; set; }
+        }
+
     }
+
+    
 }
