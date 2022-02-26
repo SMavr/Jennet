9b4612ad9ec35e1b461e62a27a3ce74965f385f2
Stam <smavrikis@benefit.gr>
2022-02-26 18:06:05 +0200

    Added some unit tests'

---
+++ Jennet.Solution/Jennet.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Xunit;
+
+namespace Jennet.Tests
+{
+    public class UnitTest1
+    {
+        string jsonString =
+@"
+{
+  ""ReRoutes"":[
+{
+      ""UpstreamPathTemplate"":""test"" 
+      ""DownstreamPathTemplate"": ""test""
+}
+  ]
+}
+";
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Jennet.Solution/Jennet.UI/RouteDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jennet.UI
+{
+    internal class RouteDTO
+    {
+        public int UpstreamPathTemplate { get; set; }
+        public int DownstreamPathTemplate { get; set; }
+        public int DownstreamScheme { get; set; }
+        public int DownstreamHostAndPorts { get; set; }
+    }
+}
