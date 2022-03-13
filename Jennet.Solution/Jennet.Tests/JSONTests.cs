b5952612ea46eeaf9b5ec2bdfad06998bf43daf9
Stam <smavrikis@benefit.gr>
2022-03-13 11:24:46 +0200

    Refactored tests

---
+++ Jennet.Solution/Jennet.Tests/JSONTests.cs
@@ -0,0 +1,19 @@
+using Jennet.Infrastructure;
+using System;
+using System.IO;
+using System.Text.Json;
+using Xunit;
+
+namespace Jennet.Tests
+{
+    public class JSONTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            string text = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,  @"..\..\..\..\Test.json"));
+
+            var test =  JsonSerializer.Deserialize<OcelotDTO>(text);
+        }
+    }
+}
